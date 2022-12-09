58233095a69b9a631640a458513725607452a785
Dongbeom Son <bumin35@naver.com>
2022-12-09 23:38:42 +0900

    pj init
    .

---
+++ float_MAC/fp16MAC.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    22:13:38 12/09/2022 
+// Design Name: 
+// Module Name:    fp16MAC 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module fp16MAC(
+    );
+
+
+endmodule
