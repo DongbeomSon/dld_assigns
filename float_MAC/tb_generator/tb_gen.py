62e3b7c8317cf370bfd186a1c957690b8692f623
Dongbeom Son <bumin35@naver.com>
2022-12-22 02:27:11 +0900

    subnormal value error
    수정필요

--- float_MAC/tb_generator/tb_gen.py
+++ float_MAC/tb_generator/tb_gen.py
@@ -11,13 +11,13 @@ h = open('ans_add.txt', 'w')
 
 delay = "#20"
 for idx in range(genNum):
-    x_f32 = random.uniform(-255,255)
+    x_f32 = random.uniform(-1,1)
     a = np.float16(x_f32)
 
     t = bin(np.float16(a).view('H'))[2:].zfill(16)
     f.write("\tA = 16'h" + hex(int(t, 2)).replace("0x","") +";\n")
 
-    x_f32 = random.uniform(-255,255)
+    x_f32 = random.uniform(-1,1)
     b = np.float16(x_f32)
     t = bin(np.float16(b).view('H'))[2:].zfill(16)
     f.write("\tB = 16'h" + hex(int(t, 2)).replace("0x","") +";\n")
